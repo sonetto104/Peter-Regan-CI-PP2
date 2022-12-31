9098308192229304c274250a4f8f320029f781f9
sonetto104 <peter_regan88@yahoo.com>
2022-12-31 17:21:29 +0000

    Create collection of audio tags. Convert this collection to array. Create function that calls random note from array. Create function that calls multiple random notes from array.

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -1,4 +1,12 @@
 let notes = document.getElementsByTagName("audio");
+let notesArray = Array.from(notes);
+let randomNote = notesArray[Math.floor(Math.random() * notesArray.length)];
+let intervalButton = document.getElementById("quaver-button");
+
+function getRandomNotes(notesArray, num) {
+    let shuffledNotesArray = notesArray.sort(()=>0.5 - Math.random());
+    return shuffledNotesArray.slice(0,num);
+}
 
 
 
