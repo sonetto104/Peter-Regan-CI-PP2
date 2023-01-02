236a5d6b0579f6f8de9acb996f458e1c836e0aea
sonetto104 <peter_regan88@yahoo.com>
2023-01-02 16:31:57 +0000

    Not a meaningful commit. Just experimenting.

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -1,4 +1,4 @@
-let notes = document.getElementsByTagName("audio");
+let notes = document.getElementsByTagName("source");
 let notesArray = Array.from(notes);
 let randomNote = notesArray[Math.floor(Math.random() * notesArray.length)];
 let intervalButton = document.getElementById("quaver-button");
@@ -7,6 +7,27 @@ function getRandomNotes(notesArray, num) {
     let shuffledNotesArray = notesArray.sort(()=>0.5 - Math.random());
     return shuffledNotesArray.slice(0,num);
 }
+
+
+function playNote() {
+
+    document.getElementById('quaver-button').addEventListener('click', );
+
+
+  //create a random number and assign to x
+  let x = getRandomNotes;
+  var soundSrc = notesArray[x];
+  //create a new instance of the audio object
+  if (randomNote) {
+      randomNote.play();
+  }
+  sound.src = soundSrc;
+  //play the sound
+  sound.play();
+}
+
+document.getElementById('quaver-button').addEventListener('click', playNote);
+
 
 
 
