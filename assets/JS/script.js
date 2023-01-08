0c01ace0fa87330864fde1728db34652625e61e2
sonetto104 <peter_regan88@yahoo.com>
2023-01-08 18:58:55 +0000

    Add event listener for opening modal. Add function for closing modal.

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -170,3 +170,10 @@ const openModal = function () {
     modal.classList.remove("hidden");
     overlay.classList.remove("hidden");
   };
+
+  openModalBtn.addEventListener("click", openModal);
+
+  const closeModal = function () {
+    modal.classList.add("hidden");
+    overlay.classList.add("hidden");
+  };
